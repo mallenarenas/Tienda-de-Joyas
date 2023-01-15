31bfe1373d50f035364717074bd81dd17bc7fa98
Mallen Arenas <mallenarenasc@gmail.com>
2023-01-14 22:08:36 -0300

    first commit

---
+++ src/db/connectionDb.js
@@ -0,0 +1,15 @@
+// connectionDb 
+const { Pool } = require("pg");
+require('dotenv').config()
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.HOST,
+    password: process.env.PASS,
+    database: process.env.DATABASE,
+    port: 5432,
+    allowExitOnIdle: true
+});
+
+
+module.exports = {pool}
